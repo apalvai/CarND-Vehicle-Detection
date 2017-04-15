dec8c76bbc4618162ef0eb558e5a1d64882e6644
apalvai <anoopreddy06@gmail.com>
2017-04-15 01:19:51 -0700

    Added color_hist.py to extract color histogram features of an image.

---
+++ src/color_hist.py
@@ -0,0 +1,46 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Define a function to compute color histogram features
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    
+    # Generating bin centers
+    bin_edges = channel1_hist[1]
+    bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
+    
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    
+    # Return the individual histograms, bin_centers and feature vector
+    return channel1_hist, channel2_hist, channel3_hist, bin_centers, hist_features
+
+def test():
+    # Read in the image
+    image = mpimg.imread('../test_images/test2.jpg')
+
+    rh, gh, bh, bincen, feature_vec = color_hist(image, nbins=32, bins_range=(0, 256))
+
+    # Plot a figure with all three bar charts
+    if rh is not None:
+        fig = plt.figure(figsize=(12,3))
+        plt.subplot(131)
+        plt.bar(bincen, rh[0])
+        plt.xlim(0, 256)
+        plt.title('R Histogram')
+        plt.subplot(132)
+        plt.bar(bincen, gh[0])
+        plt.xlim(0, 256)
+        plt.title('G Histogram')
+        plt.subplot(133)
+        plt.bar(bincen, bh[0])
+        plt.xlim(0, 256)
+        plt.title('B Histogram')
+        fig.tight_layout()
+        plt.show()
+
+test()
