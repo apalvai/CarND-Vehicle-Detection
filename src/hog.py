d60eb0a72e1a975d4aa455ac0dd49459b69efc6a
apalvai <anoopreddy06@gmail.com>
2017-04-15 00:53:09 -0700

    Added hog.py to extract hog features from an image to detect shapes of cars.

---
+++ src/hog.py
@@ -0,0 +1,39 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import cv2
+from skimage.feature import hog
+
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
+    # Use skimage.hog() to get both features and a visualization
+    features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell), cells_per_block=(cell_per_block, cell_per_block), visualise=vis, feature_vector=feature_vec)
+    if vis == True:
+        return features, hog_image
+    else:
+        return features
+
+def test():
+    # Read in the image
+    image = mpimg.imread('../vehicles/GTI_Far/image0956.png')
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    
+    # Define HOG parameters
+    orient = 9
+    pix_per_cell = 8
+    cell_per_block = 2
+    
+    # Call our function with vis=True to see an image output
+    features, hog_image = get_hog_features(gray, orient,
+                                           pix_per_cell, cell_per_block,
+                                           vis=True, feature_vec=False)
+    
+    # Plot the examples
+    fig = plt.figure()
+    plt.subplot(121)
+    plt.imshow(image, cmap='gray')
+    plt.title('Example Car Image')
+    plt.subplot(122)
+    plt.imshow(hog_image, cmap='gray')
+    plt.title('HOG Visualization')
+    plt.show()
+
+test()
